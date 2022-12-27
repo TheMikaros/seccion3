1e52d1575ca99ae40cecdcad3f4d4e5ca94c1951
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-27 13:35:48 -0600

    Initial commit

---
+++ Ej1a4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+//            1 - Cree un proyecto de consola que imprima su nombre en la ventana de la consola.
+
+//2 - Cree un proyecto de consola de.Net Framework en su versión 4.6 y otro de.Net Core 2.1
+
+//3 - En el proyecto del punto 1, pon un punto de interrupción antes de mostrar tu nombre por consola.
+
+//4 - En el proyecto del punto 1, pide por consola que el usuario introduzca su nombre y muestra "Hola, <nombre>"
+
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
---
+++ Ej5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Pedir por consola un nombre de persona y el nombre de una ciudad
+            //(no hace falta que sean reales o comprobarlos) y mostrar por pantalla,
+            //el siguiente mensaje “Hola ” < nombre > ” bienvenido a ” < ciudad >
+
+            Console.WriteLine("Input name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Input city:");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Hola " + name + " bienbenido a " + city);
+
+        }
+    }
+}
